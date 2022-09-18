613663dbe45897bb5eb089f3880171a7a12d88f4
Zhaorui Wang <748339596@qq.com>
2022-09-18 17:50:35 +0800

    ..

---
+++ bit2dec.m
@@ -0,0 +1,16 @@
+fid=fopen("915","r")
+[data,count]=fread(fid,10000,"uint8")
+%% 
+
+a=data(1:4,1)
+a(3,1)
+b=a(1,1)*8+a(2,1)*4+a(3,1)*2+a(4,1)*1
+%% 
+i=1
+data_hex=zeros(2500,1)
+a=zeros(4,1)
+while i<2501
+    a=data((i-1)*4+1:i*4,1);
+    data_hex(i,1)=a(1,1)*8+a(2,1)*4+a(3,1)*2+a(4,1)*1;
+    i=1+i;
+end
