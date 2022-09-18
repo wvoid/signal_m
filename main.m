d74a20c98be05059f60fb8238d69736842cb8bb6
Zhaorui Wang <748339596@qq.com>
2022-09-18 16:14:04 +0800

    .

---
+++ main.m
@@ -0,0 +1,29 @@
+
+%% 
+
+data_complex=read_complex_binary('915mhz_sp40mhz_bw20mhz.data',40e6);
+%% 
+signal_i=real(data_complex);
+signal_q=imag(data_complex);
+%% 
+subplot(2,1,1)
+plot(real(data_complex))
+title('I')
+xlabel('samples at 10mhz')
+ylabel('Amplitude');
+subplot(2,1,2)
+plot(imag(data_complex))
+title('Q')
+xlabel('samples at 10mhz')
+ylabel('Amplitude')
+%% 
+figure(2)
+spectrogram(data_complex(36.2e6:36.4e6),1000)
+%% 
+figure(3)
+fin_q = fft(signal_q);
+fin_i = fft(signal_i);
+nnz(isnan(fin_q))         
+nnz(isnan(fin_i))          
+subplot(1,2,1); plot(abs(fin_q)); title('q');
+subplot(1,2,2); plot(abs(fin_i)); title('i');
---
+++ read_complex_binary.m
@@ -0,0 +1,18 @@
+function v=read_complex_binary(filename,count)
+m=nargchk(1,2,nargin);
+if (m)
+    error(m);
+end
+if (nargin<2)
+    count=Inf;
+end
+f=fopen(filename,'rb');
+if(f<0)
+    v=0;
+else
+    t=fread(f,[2,count],'float');
+    fclose(f);
+    v=t(1,:)+t(2,:)*i;
+    [r,c]=size(v);
+    v=reshape(v,c,r);
+end
