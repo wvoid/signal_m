907249d746636fedb9b626878ee3325712a5b2e8
Zhaorui Wang <748339596@qq.com>
2022-09-27 17:18:25 +0800

    9.27,17.18

---
+++ CoarseSync.m
@@ -0,0 +1,26 @@
+function [preambleIdx,x] = CoarseSync(stdPreamble,data)
+%% 粗同步
+%INPUT:
+% stdPreamble 标准前导码
+% data 信号数据
+%OUTPUT:
+% preambleIdx 前导码下标
+%% 归一化幅值
+A_data_max=max(abs(real(data)));
+data=data/A_data_max;
+A_stdP_max=max(abs(real(stdPreamble)));
+stdPreamble=stdPreamble/A_stdP_max;
+%%
+
+len1 = length(stdPreamble); % 前导码长度
+len2 = length(data); % 信号长度
+col = len2-len1+1; % 遍历次数
+x = zeros(1,col); % 结果数组
+
+for cur = 1:col
+    R = corrcoef(stdPreamble, data(cur:cur+len1-1, 1)); % 计算相关系数
+    x(cur) = abs(R(1, 2));
+end
+[~, preambleIdx]= max(x);  % 取最大相关系数对应的结果的下标
+
+end
--- main.m
+++ main.m
@@ -1,20 +1,20 @@
 
-%% 
+%% 读文件
 
-data_complex=read_complex_binary('915_sp10mhz',40e6);
+data_complex=read_complex_binary('915mhz_sp1mhz_preamble',40e6);
 %% 
 signal_i=real(data_complex);
 signal_q=imag(data_complex);
-%% 
+%% 画出iq两路信号波形
 subplot(2,1,1)
 plot(real(data_complex))
 title('I')
-xlabel('samples at 10mhz')
+xlabel('samples at 2mhz')
 ylabel('Amplitude');
 subplot(2,1,2)
 plot(imag(data_complex))
 title('Q')
-xlabel('samples at 10mhz')
+xlabel('samples at 2mhz')
 ylabel('Amplitude')
 %% 
 figure(2)
@@ -28,12 +28,12 @@ nnz(isnan(fin_i))
 subplot(1,2,1); plot(abs(fin_q)); title('q');
 subplot(1,2,2); plot(abs(fin_i)); title('i');
 %% 
-prb = signal_i(8.69e5:8.72e5,1);
+preamble_test=data_complex(3.2479e7:3.2484e7-1,1);
 %% 
-prbdet = comm.PreambleDetector(prb,'Input','Symbol','Detections','First','Threshold',100);
-pkt=signal_i;
-idx=prbdet(pkt);
+[idx,x]=CoarseSync(preamble_test,data_complex(10e6:20e6,1));
 %% 
-a=[1 3 4 5];
-b=[1 3 4 5];
-corrcoef(a,b)
+[val,i]=sort(x,'descend');
+%% 
+v=val(1:1000);
+idx=i(1:1000)
+
